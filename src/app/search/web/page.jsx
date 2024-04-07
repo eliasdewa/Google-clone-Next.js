43f9cfe76f3dcc5bc3f9a25afe285ebbe59b4f49
Elias-Dewa <elias.dewa19@gmail.com>
2024-04-07 12:37:22 -0700

    bug is fixed to handle error

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -4,7 +4,16 @@ export default async function WebSearchPage({searchParams}) {
   // create a request for api
   const res = await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}`);
 
-  if (!res.ok) throw new Error('Something went wrong')
+  if (!res.ok) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">Something went wrong!</h1>
+        <p className="text-lg">
+          <Link href="/" className='text-blue-500 cursor-pointer hover:underline'>Try again</Link>
+        </p>
+      </div>
+    )
+  }
   const data = await res.json();
   const results = data.items;
   if (!results) {
