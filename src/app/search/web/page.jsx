8d7e780de7e455801e376fb03bd249003fc71b95
Elias-Dewa <elias.dewa19@gmail.com>
2024-04-08 05:24:43 -0700

    create search results components

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,3 +1,4 @@
+import WebSearchResults from "@/app/components/WebSearchResults";
 import Link from "next/link";
 
 export default async function WebSearchPage({searchParams}) {
@@ -28,7 +29,7 @@ export default async function WebSearchPage({searchParams}) {
   }
   return (
     <div>
-      {results && results.map(result => <h1>{result.title}</h1>)}
+      {results && <WebSearchResults results={data}/>}
     </div>
   )
 }
